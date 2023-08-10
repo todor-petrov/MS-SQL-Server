cf858f1f9f24f2ec404301f1862d9daa54731bcc
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-08-10 06:57:19 +0300

    Add files via upload

---
+++ Projects/Bitbucket/SQL_Queries.sql
@@ -0,0 +1,181 @@
+/*CREATE DATABASE Bitbucket
+GO
+USE Bitbucket*/
+
+--01. DDL
+/*
+CREATE TABLE Users (
+					Id INT PRIMARY KEY IDENTITY,
+					Username VARCHAR(30) NOT NULL,
+					[Password] VARCHAR(30) NOT NULL,
+					Email VARCHAR(50) NOT NULL
+					)
+CREATE TABLE Repositories (
+						   Id INT PRIMARY KEY IDENTITY,
+						   [Name] VARCHAR(30) NOT NULL
+						   )
+CREATE TABLE RepositoriesContributors (
+									   RepositoryId INT FOREIGN KEY REFERENCES Repositories(Id),
+									   ContributorId INT FOREIGN KEY REFERENCES Users(Id)
+									   PRIMARY KEY(RepositoryId, ContributorId)
+									   )
+CREATE TABLE Issues (
+					 Id INT PRIMARY KEY IDENTITY,
+					 Title VARCHAR(255) NOT NULL,
+					 IssueStatus VARCHAR(6) NOT NULL,
+					 RepositoryId INT FOREIGN KEY REFERENCES Repositories(Id) NOT NULL,
+					 AssigneeId INT FOREIGN KEY REFERENCES Users(Id) NOT NULL
+					 )
+CREATE TABLE Commits (
+					  Id INT PRIMARY KEY IDENTITY,
+					  [Message] VARCHAR(255) NOT NULL,
+					  IssueId INT FOREIGN KEY REFERENCES Issues(Id),
+					  RepositoryId INT FOREIGN KEY REFERENCES Repositories(Id) NOT NULL,
+					  ContributorId INT FOREIGN KEY REFERENCES Users(Id) NOT NULL
+					  )
+CREATE TABLE Files (
+					Id INT PRIMARY KEY IDENTITY,
+					[Name] VARCHAR(100) NOT NULL,
+					Size DECIMAL(15, 2) NOT NULL,
+					ParentId INT FOREIGN KEY REFERENCES Files(Id),
+					CommitId INT FOREIGN KEY REFERENCES Commits(Id) NOT NULL
+					)
+*/
+
+--02. Insert
+/*
+INSERT INTO Files ([Name], Size, ParentId, CommitId)
+VALUES ('Trade.idk', 2598.0, 1, 1),
+	   ('menu.net', 9238.31, 2, 2),
+	   ('Administrate.soshy', 1246.93, 3, 3),
+	   ('Controller.php', 7353.15, 4, 4),
+	   ('Find.java', 9957.86, 5, 5),
+	   ('Controller.json', 14034.87, 3, 6),
+	   ('Operate.xix', 7662.92, 7, 7)
+
+INSERT INTO Issues (Title, IssueStatus, RepositoryId, AssigneeId)
+VALUES ('Critical Problem with HomeController.cs file', 'open', 1, 4),
+		('Typo fix in Judge.html', 'open', 4, 3),
+		('Implement documentation for UsersService.cs', 'closed', 8, 2),
+		('Unreachable code in Index.cs', 'open', 9, 8)
+*/
+
+--03. Update
+/*
+UPDATE Issues
+   SET IssueStatus = 'closed'
+ WHERE AssigneeId = 6
+ */
+
+ --04. Delete
+/*
+DELETE FROM child
+FROM RepositoriesContributors AS child
+INNER JOIN Repositories AS parent ON child.RepositoryId = parent.Id
+WHERE parent.[Name] = 'Softuni-Teamwork'
+
+DELETE FROM child
+FROM Issues AS child
+INNER JOIN Repositories AS parent ON child.RepositoryId = parent.Id
+WHERE parent.[Name] = 'Softuni-Teamwork'
+
+DELETE FROM child
+FROM Files AS child
+INNER JOIN Commits AS parent ON child.CommitId = parent.Id
+INNER JOIN Repositories AS r ON parent.RepositoryId = r.Id
+WHERE r.[Name] = 'Softuni-Teamwork'
+
+DELETE FROM child
+FROM Commits AS child
+INNER JOIN Repositories AS parent ON child.RepositoryId = parent.Id
+WHERE parent.[Name] = 'Softuni-Teamwork'
+
+DELETE FROM parent
+FROM Repositories AS parent
+WHERE parent.[Name] = 'Softuni-Teamwork'
+*/
+
+--05. Commits
+/*
+  SELECT Id, [Message], RepositoryId, ContributorId
+	FROM Commits
+ORDER BY Id, [Message], RepositoryId, ContributorId
+*/
+
+--06. Front-end
+/*
+  SELECT Id, [Name], Size
+	FROM Files
+   WHERE Size > 1000 AND [Name] LIKE '%html%'
+ORDER BY Size DESC, Id, [Name]
+*/
+
+--07. Issue Assignment
+/*
+   SELECT i.Id, CONCAT(u.Username, ' : ', i.Title) AS IssueAssignee
+	 FROM Issues AS i
+LEFT JOIN Users AS u
+	   ON i.AssigneeId = u.Id
+ ORDER BY i.Id DESC, i.AssigneeId
+*/
+
+--08. Single Files
+/*
+  SELECT Id, [Name], CONCAT(Size, 'KB') AS Size
+	FROM Files
+   WHERE Id NOT IN (
+				  SELECT DISTINCT f.Id
+				    FROM Files AS f
+				    JOIN Files AS p
+					  ON f.Id = p.ParentId
+				  )
+ORDER BY Id, [Name], Size DESC
+*/
+
+--09. Commits in Repositories
+/*
+   SELECT TOP 5 r.Id, r.[Name], COUNT(r.[Name]) AS Commits
+	 FROM Repositories as r
+LEFT JOIN Commits as c
+	   ON r.Id = c.RepositoryId
+	 JOIN RepositoriesContributors AS rc
+	   ON r.Id = rc.RepositoryId
+ GROUP BY r.Id, r.[Name]
+ ORDER BY Commits DESC, r.Id, r.[Name]
+*/
+
+--10. Average Size
+/*
+	SELECT u.Username, AVG(f.Size) AS Size
+	  FROM Commits AS c
+INNER JOIN Users AS u
+		ON c.ContributorId = u.Id
+INNER JOIN Files AS f
+		ON c.Id = f.CommitId
+  GROUP BY u.Username
+  ORDER BY Size DESC, u.Username
+*/
+
+--11. All User Commits
+/*
+CREATE FUNCTION udf_AllUserCommits (@username VARCHAR(30))
+RETURNS INT AS
+BEGIN
+	RETURN (
+	SELECT COUNT(c.Id)
+	  FROM Users AS u
+INNER JOIN Commits AS c
+		ON u.Id = c.ContributorId
+	  WHERE u.Username = @username
+			 )
+END
+*/
+
+--12. Search for Files
+/*
+CREATE PROCEDURE usp_SearchForFiles (@fileExtension VARCHAR(30))
+			  AS
+		  SELECT Id, [Name], CONCAT(Size, 'KB') AS Size
+			FROM Files
+		   WHERE [Name] LIKE '%' + @fileExtension
+*/
