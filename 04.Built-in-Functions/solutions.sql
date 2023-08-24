ee9703ae9b6b091aaa9d73fb02d20877f9e41428
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-08-24 06:55:00 +0300

    Add files via upload

--- 04.Built-in-Functions/solutions.sql
+++ 04.Built-in-Functions/solutions.sql
@@ -30,4 +30,45 @@ ORDER BY [Name]
    WHERE LEFT([Name], 1) IN ('M', 'K', 'B', 'E')
 ORDER BY [Name]
 
-/*  */
+/* 07. Find Towns Not Starting With */
+  SELECT *
+    FROM [Towns]
+   WHERE LEFT([Name], 1) NOT IN ('R', 'B', 'D')
+ORDER BY [Name]
+
+/* 08. Create View Employees Hired After 2000 Year */
+CREATE VIEW [V_EmployeesHiredAfter2000] AS
+	 SELECT [FirstName], [LastName]
+	   FROM [Employees]
+	  WHERE DATEPART(YEAR, [HireDate]) > 2000
+
+/* 09. Length of Last Name */
+SELECT [FirstName], [LastName]
+  FROM [Employees]
+ WHERE LEN(LastName) = 5
+
+/* 10. Rank Employees by Salary */
+  SELECT [EmployeeID], [FirstName], [LastName], [Salary],
+	     DENSE_RANK() OVER (PARTITION BY [Salary] ORDER BY [EmployeeID])
+    FROM [Employees]
+   WHERE [Salary] BETWEEN 10000 AND 50000
+ORDER BY [Salary] DESC
+
+/* 11. Find All Employees with Rank 2 */
+SELECT *
+   FROM (
+		  SELECT [EmployeeID], [FirstName], [LastName], [Salary],
+				 DENSE_RANK() OVER(PARTITION BY [Salary] ORDER BY [EmployeeID])
+			  AS [Rank]
+			FROM [Employees]
+		   WHERE [Salary] BETWEEN 10000 AND 50000
+	     )
+	  AS [RankingSubquery]
+   WHERE [Rank] = 2
+ORDER BY [Salary] DESC
+
+/* 12. Countries Holding 'A' 3 or More Times */
+  SELECT [CountryName], [IsoCode]
+    FROM [Countries]
+   WHERE LOWER([CountryName]) LIKE '%a%a%a%'
+ORDER BY [IsoCode]
