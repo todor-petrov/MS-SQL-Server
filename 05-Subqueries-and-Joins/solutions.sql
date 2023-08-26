585bb9657672675f8c07679ab658de0987f7075e
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-08-26 09:16:04 +0300

    Add files via upload

--- 05-Subqueries-and-Joins/solutions.sql
+++ 05-Subqueries-and-Joins/solutions.sql
@@ -21,3 +21,11 @@ LEFT JOIN [Departments] AS [d]
 	   ON [e].[DepartmentID] = [d].[DepartmentID]
 	WHERE [d].[Name] = 'Sales'
  ORDER BY [e].[EmployeeID]
+
+/* 04. Employee Departments */
+SELECT TOP (5) [EmployeeID], [FirstName], [Salary], [d].[Name] AS DepartmentName
+	  FROM [Employees] AS [e]
+INNER JOIN [Departments] AS [d]
+		ON [e].[DepartmentID] = [d].[DepartmentID]
+	 WHERE [e].[Salary] > 15000
+  ORDER BY [d].[DepartmentID]
