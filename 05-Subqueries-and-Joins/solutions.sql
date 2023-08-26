6673cfc749dff18de0937862c7c35d4774716bde
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-08-26 18:58:35 +0300

    Add files via upload

--- 05-Subqueries-and-Joins/solutions.sql
+++ 05-Subqueries-and-Joins/solutions.sql
@@ -29,3 +29,11 @@ INNER JOIN [Departments] AS [d]
 		ON [e].[DepartmentID] = [d].[DepartmentID]
 	 WHERE [e].[Salary] > 15000
   ORDER BY [d].[DepartmentID]
+
+/* 05. Employees Without Projects */
+SELECT TOP (3) [e].[EmployeeID], [e].[FirstName]
+	  FROM Employees AS [e]
+ LEFT JOIN [EmployeesProjects] AS [ep]
+		ON [e].[EmployeeID] = [ep].EmployeeID
+	 WHERE [ep].[EmployeeID] IS NULL
+  ORDER BY [e].[EmployeeID]
