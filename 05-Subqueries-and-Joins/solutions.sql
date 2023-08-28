4155057cc351863d8f0a334e8131bedc5528f6e2
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-08-28 13:37:57 +0300

    Add files via upload

--- 05-Subqueries-and-Joins/solutions.sql
+++ 05-Subqueries-and-Joins/solutions.sql
@@ -79,3 +79,14 @@ INNER JOIN [Employees] AS [m]
 		ON [e].[ManagerID] = [m].[EmployeeID]
 	 WHERE [m].[EmployeeID] IN (3, 7)
   ORDER BY [e].[EmployeeID]
+
+/* 10. Employees Summary */
+	SELECT TOP (50) [e].[EmployeeID]
+		   ,CONCAT([e].[FirstName], ' ', [e].[LastName]) AS [EmployeeName]
+		   ,CONCAT([m].[FirstName], ' ', [m].[LastName]) AS [ManagerName]
+		   ,[d].[Name] AS [DepartmentName]
+	  FROM [Employees] AS [e]
+INNER JOIN [Employees] AS [m]
+		ON [e].[ManagerID] = [m].[EmployeeID]
+ LEFT JOIN [Departments] AS [d]
+		ON [e].[DepartmentID] = [d].[DepartmentID]
