651cf4f7dd11a17b68d4c55ea5cc62be7e5ed722
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-08-28 12:01:17 +0300

    Add files via upload

--- 05-Subqueries-and-Joins/solutions.sql
+++ 05-Subqueries-and-Joins/solutions.sql
@@ -68,3 +68,14 @@ SELECT [e].[EmployeeID], [e].[FirstName],
   LEFT JOIN [Projects] as [p]
     ON [ep].[ProjectID] = [p].[ProjectID]
  WHERE [e].[EmployeeID] = 24
+
+/* 09. Employee Manager */
+    SELECT [e].[EmployeeID],
+		   [e].[FirstName],
+		   [m].[EmployeeID] AS [ManagerID],
+		   [m].[FirstName] AS [ManagerName]
+	  FROM [Employees] AS [e]
+INNER JOIN [Employees] AS [m]
+		ON [e].[ManagerID] = [m].[EmployeeID]
+	 WHERE [m].[EmployeeID] IN (3, 7)
+  ORDER BY [e].[EmployeeID]
