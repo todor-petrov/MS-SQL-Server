40b748c70433d76ac66844929e96d176260be207
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-09-25 16:26:18 +0300

    Add files via upload

--- 07-Functions-and-Stored-Procedures/solutions.sql
+++ 07-Functions-and-Stored-Procedures/solutions.sql
@@ -36,7 +36,7 @@ EXEC dbo.usp_GetTownsStartingWith 'b'
 
 -- 04. Employees from Town
 GO
-CREATE PROC usp_GetEmployeesFromTown @Town VARCHAR(100)
+CREATE PROC usp_GetEmployeesFromTown @Town VARCHAR(50)
 		 AS
 	  BEGIN
 			SELECT e.FirstName
@@ -50,3 +50,19 @@ CREATE PROC usp_GetEmployeesFromTown @Town VARCHAR(100)
 		END
 
 EXEC dbo.usp_GetEmployeesFromTown 'Sofia'
+
+-- 05. Salary Level Function
+GO
+CREATE FUNCTION ufn_GetSalaryLevel (@salary DECIMAL(18,4))
+		RETURNS VARCHAR(8)
+			 AS
+		  BEGIN
+				DECLARE @salary_range VARCHAR(8)
+				SET @salary_range = 'Average'
+				IF @salary < 30000 SET @salary_range = 'Low'
+				ELSE IF @salary > 50000 SET @salary_range = 'High'
+		 RETURN @salary_range
+			END
+GO
+
+SELECT dbo.ufn_GetSalaryLevel(13500)
