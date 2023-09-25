72e526f5b976d6b09d8a039fbbb76d600d799e86
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-09-25 13:51:58 +0300

    Add files via upload

--- 07-Functions-and-Stored-Procedures/solutions.sql
+++ 07-Functions-and-Stored-Procedures/solutions.sql
@@ -10,3 +10,14 @@ CREATE PROC usp_GetEmployeesSalaryAbove35000
 
 EXEC dbo.usp_GetEmployeesSalaryAbove35000
 
+-- 02. Employees with Salary Above Number
+GO
+CREATE PROC usp_GetEmployeesSalaryAboveNumber @Number DECIMAL(18, 4)
+		 AS
+	  BEGIN
+			SELECT FirstName,LastName
+			  FROM Employees
+			 WHERE Salary >= @Number
+		END
+
+EXEC dbo.usp_GetEmployeesSalaryAboveNumber @Number = 48100
